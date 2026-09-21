93ad6a5650e777e45160588ec9c693d389bf6d4b
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-09-21 10:59:25 +0800

    instance method

--- posttest-1/posttest-1.py
+++ posttest-1/posttest-1.py
@@ -11,6 +11,18 @@ class ProdukOptik:
         self.__stok = stok
         ProdukOptik.produk_terdaftar += 1
 
+    def tampil_detail(self):
+        print(f'| [{self.id_sku:<15}] {self.nama_produk:<35} | Kat: {self.kategori:<5} | Harga: Rp{self.harga:<10,.0f} | Stok: {self.stok:<3} |')
+
+    def kurangi_stok(self, jumlah):
+        if jumlah <= self.__stok:
+            self.__stok -= jumlah
+            return True
+        else:
+            print(f'Stok {self.nama_produk} tidak cukup, (Sisa : {self.__stok})')
+            return False
+
+
 
 class Inventory:
     nama_sistem = 'Optik Inventory System'
@@ -21,6 +33,19 @@ class Inventory:
         self.nama_cabang = nama_cabang
         self.daftar_barang = []
         self.__pin_akses = '018'
+
+    def tampil_stok(self):
+        print(f'\n===================== STOK OPTIK CAHAYA ({self.nama_cabang.upper()}) =========================')
+        if not self.daftar_barang:
+            print('Belum ada produk yang terdaftar')
+        else:
+            for produk in self.daftar_barang:
+                produk.tampil_detail()
+        print('=' * 104 + '\n')
+
+    def tambah_produk(self, produk):
+        self.daftar_barang.append(produk)
+        print(f'Berhasil menambahkan {produk.nama_produk} ke inventory {self.nama_cabang}')
 
 
 
@@ -36,4 +61,27 @@ class Transaksi:
         self.status_pembayaran = 'Pending'
         self.__total_bayar = 0
 
+    def cetak_nota(self):
+        print('\n' + '=' * 75)
+        print(f'               NOTA PEMBELIAN - {Transaksi.nama_optik.upper()}')
+        print('=' * 75)
+        print(f'ID Transaksi : {self.id_transaksi}')
+        print(f'Pelanggan    : {self.nama_pelanggan}')
+        print('-' * 75)
+        print(f"{'SKU CODE':<15} | {'ITEM':<35} | {'QTY':<3} | {'SUBTOTAL':<12}")
+        print('-' * 75)
+        kacamata = False
+        for item in self.item_pembelian:
+            prod = item['produk']
+            print(f"{prod.id_sku:<15} | {prod.nama_produk:<35} | {item['qty']:<3} | Rp{item['subtotal']:<10,.0f}")
+            if prod.kategori in ['FRM', 'LNS']:
+                kacamata = True
+        print('-' * 75)
+        print(f'TOTAL BELANJA : Rp{self.total_bayar:,.0f}')
+        if kacamata:
+            print('[FREE BONUS]:')
+            for bonus in Transaksi.bonus:
+                print(f'  + 1x {bonus}')
+        print('=' * 75 + '\n')
+        Transaksi.transaksi_selesai += 1
 
