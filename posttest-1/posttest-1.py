0807c2a3e124728bfa25bbba7f2042c864226694
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-09-21 10:35:23 +0800

    3 class, 3 attribut class

---
+++ posttest-1/posttest-1.py
@@ -0,0 +1,14 @@
+class ProdukOptik:
+    nama_optik = 'Optik Cahaya'
+    produk_terdaftar = 0
+    kategori_produk = ['FRM', 'LNS', 'AKS']
+
+class Inventory:
+    nama_sistem = 'Optik Inventory System'
+    max_gudang = 1000
+    lokasi_gudang = 'Gudang Pusat Samarinda'
+
+class Transaksi:
+    nama_optik = 'Optik Cahaya'
+    transaksi_selesai = 0
+    bonus = ['Kotak Kacamata Hardcase Optik Cahaya', 'Lap Kain Microfiber Premium']
